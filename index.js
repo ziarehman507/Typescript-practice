44c745ccbdc012d674b0880903d5445413e48fdd
zia ur rehman <114823425+ziarehman507@users.noreply.github.com>
2024-03-05 10:49:58 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var userName = "Noman";
-var user_name = "Owais";
+var userName = "jameel";
+var user_name = "imran";
 var UserName = "Khan";
-var isUserExist = "true";
-var is_user_Exist = "faise";
-var IsUserExist = 1;
+var isUserExist = true;
+var is_user_Exist = false;
+var IsUserExist = 10;
 console.log(userName, user_name, UserName, isUserExist, is_user_Exist, IsUserExist);
+/// Arrays => []  0
+var students = ["Qasim", "Amir", "Shahid", "Umer"];
+console.log(students[2]);
--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
-let userName = "Noman"
-let user_name = "Owais"
+let userName = "jameel"
+let user_name = "imran"
 let UserName = "Khan"
 
-let isUserExist = "true"
-let is_user_Exist = "faise"
-let IsUserExist = 1
+let isUserExist = true
+let is_user_Exist = false
+let IsUserExist = 10
 
 
 console.log(userName, user_name,UserName,isUserExist,is_user_Exist,IsUserExist);
+
+/// Arrays => []  0
+  
+let students = ["Qasim", "Amir", "Shahid","Umer"]
+ console.log(students[2])
