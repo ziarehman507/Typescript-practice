e4eda4468615607b3b0ee0c68ed8bc3757c2b6b8
zia ur rehman <114823425+ziarehman507@users.noreply.github.com>
2024-03-05 10:10:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var userName = "Noman";
+var user_name = "Owais";
+var UserName = "Khan";
+var isUserExist = "true";
+var is_user_Exist = "faise";
+var IsUserExist = 1;
+console.log(userName, user_name, UserName, isUserExist, is_user_Exist, IsUserExist);
---
+++ index.ts
@@ -0,0 +1,10 @@
+let userName = "Noman"
+let user_name = "Owais"
+let UserName = "Khan"
+
+let isUserExist = "true"
+let is_user_Exist = "faise"
+let IsUserExist = 1
+
+
+console.log(userName, user_name,UserName,isUserExist,is_user_Exist,IsUserExist);
